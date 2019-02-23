48e44b07f82798a748149e9a93e2da8606acb2de
dnull7 <null2@illinois.edu>
2019-02-23 01:30:34 -0600

    bible api

--- MYSCRIPT.py
+++ MYSCRIPT.py
@@ -1,3 +1,4 @@
-import requests
-response = requests.get("http://labs.bible.org/api/?passage=John+3:16-17")
-print(response.content)
+#import requests
+#response = requests.get("http://labs.bible.org/api/?passage=John+3:16-17")
+#print(response.content)
+print(5)
