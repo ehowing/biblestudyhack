1016c798831553a9391f2455a257cd679d093191
dnull7 <null2@illinois.edu>
2019-02-23 01:06:57 -0600

    bible api

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const Http = new XMLHttpRequest();
+	const url='http://labs.bible.org/api/?passage=John+3:16-17';
+	Http.open("GET", url);
+	Http.send();
+	Http.onreadystatechange=(e)=>{
+	console.log(Http.responseText)
+	}
 window.alert("okay");
